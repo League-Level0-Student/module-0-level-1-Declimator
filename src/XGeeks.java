49f6e0274c07f0d91cf4e54113680c9f43a748cb
GabrielZ1 <League@AdministorsiMac.attlocal.net>
2017-10-13 18:18:09 -0700

    XGeeks IP

--- src/XGeeks.java
+++ src/XGeeks.java
@@ -15,11 +15,18 @@ public class XGeeks {
 public static void main(String[] args) {
 
 		// 1. Save the superpower for each person in a variable.
-
+String superpower = JOptionPane.showInputDialog("What's Your SuperPower");
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+String name = JOptionPane.showInputDialog("What's Your name?");
 		// 3. Show the superpower in a pop-up, depending on the name entered. 
-
+String ask = JOptionPane.showInputDialog("Who do you want to learn about");
+JOptionPane.showMessageDialog(null, name + "'s superower is " + superpower + "!");
+String superpowera = JOptionPane.showInputDialog("What's Your SuperPower");
+String superpowerb = JOptionPane.showInputDialog("What's Your SuperPower");
+String superpowerc = JOptionPane.showInputDialog("What's Your SuperPower");
+String namea = JOptionPane.showInputDialog("What's Your name?");
+String nameb = JOptionPane.showInputDialog("What's Your name?");
+String namec = JOptionPane.showInputDialog("What's Your name?");
 	}
 }
 
